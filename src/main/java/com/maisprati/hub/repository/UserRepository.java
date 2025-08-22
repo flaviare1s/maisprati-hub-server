5b9bfc3e0415e9a0ca5e6c7fbe30dfdfcd1f74f8
Vitória Barbosa <barbosa.vitoriags@gmail.com>
2025-08-22 03:05:23 -0300

    feat: add UserRepository interface
    Cria a interface UserRepository estendendo MongoRepository para manipulação dos dados de usuários.

---
+++ src/main/java/com/maisprati/hub/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.maisprati.hub.repository;
+
+import com.maisprati.hub.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	Optional<User> findByEmail(String email);
+	
+	boolean existsByEmail(String email);
+}
