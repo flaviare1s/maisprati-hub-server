5f0ba36ca57d5097b01722f6ded1ac74be3b3b6a
flaviare1s <flaviarster@gmail.com>
2025-08-27 08:02:01 -0300

    refactor: replace turma with groupClass

--- src/main/java/com/maisprati/hub/model/User.java
+++ src/main/java/com/maisprati/hub/model/User.java
@@ -39,7 +39,7 @@ public class User {
 	private UserType type;
 
 	private String whatsapp;
-	private String turma;
+	private String groupClass;
 	private Boolean hasGroup;
 	private Boolean wantsGroup;
 	private Boolean isFirstLogin;
