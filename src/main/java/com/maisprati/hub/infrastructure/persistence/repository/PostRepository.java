6327de73fb72ae2642b40a1dbcf13be2b8bca536
flaviare1s <flaviarster@gmail.com>
2025-09-10 10:14:00 -0300

    feat: add PostRepository

---
+++ src/main/java/com/maisprati/hub/infrastructure/persistence/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.maisprati.hub.infrastructure.persistence.repository;
+
+import com.maisprati.hub.domain.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findAllByAuthorId(String authorId);
+    List<Post> findAllByIsActiveTrueOrderByCreatedAtDesc();
+}
