0632334cfc512a6e2be9d9575a3c1a8dadaea154
Vitória Barbosa <barbosa.vitoriags@gmail.com>
2025-09-28 01:25:34 -0300

    chore: add TODO for frontend reset-password URL in EmailService
    Adicionada uma nota para substituir a URL do host local pela rota real do frontend durante a integração.

--- src/main/java/com/maisprati/hub/application/service/EmailService.java
+++ src/main/java/com/maisprati/hub/application/service/EmailService.java
@@ -12,12 +12,13 @@ public class EmailService {
 	private final JavaMailSender mailSender;
 	
 	public void sendPasswordResetEmail(String to, String token) {
-		String resetUrl = "http://localhost:8080/reset-password?token=" + token;
+		// TODO: URL do front que vai renderizar o formulário
+		String resetUrl = "http://localhost:5173/reset-password?token=" + token;
 		
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
 		message.setSubject("Password Reset Request");
-		message.setText("Clique no link para resetar sua senha: " + resetUrl);
+		message.setText("Clique no link para redefinir sua senha: " + resetUrl);
 		mailSender.send(message);
 	}
 }
