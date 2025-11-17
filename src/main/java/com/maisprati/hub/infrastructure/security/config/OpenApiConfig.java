9ca2a66c5f3bed1bda3db97242a14bd7e2914d78
flaviare1s <flaviarster@gmail.com>
2025-11-17 06:41:41 -0300

    feat: OpenApiConfig

---
+++ src/main/java/com/maisprati/hub/infrastructure/security/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.maisprati.hub.infrastructure.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Demonstration API",
+                version = "v1",
+                description = "API de demonstração com autenticação JWT e recursos de usuários, times, agendamentos, horários, notificações, posts, comentários e quadros de progresso dos projetos",
+                contact = @Contact(name = "+praTiHub", email = "maisprati.hub@gmail.com")
+        ),
+        security = {@SecurityRequirement(name = "bearerAuth")}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
